4bc59061957244ea3920475683f4d6714de5c40f
damianf2022 <damianf2022@gmail.com>
2023-10-06 11:36:31 -0300

    deploy

--- api/config/database.js
+++ api/config/database.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 
-const connectDatabase = () => {
-  mongoose.set('strictQuery', false);
-  mongoose
-    .connect(process.env.MONGODB_URI, {
+const connectDatabase = async () => {
+  try {
+    mongoose.set('strictQuery', false);
+    const connection = await mongoose.connect(process.env.DB_LOCAL_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       // strictQuery: false,
-    })
-    .then((con) => {
-      console.log(
-        `MongoDB Database connected with HOST: ${con.connection.host}`
-      );
     });
+    console.log(`MongoDB Database connected with HOST: ${connection.connection.host}`);
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error; // Re-lanza el error para que pueda ser manejado en la llamada a esta función
+  }
 };
+
 module.exports = connectDatabase;
--- api/server.js
+++ api/server.js
@@ -1,14 +1,24 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 const connectDatabase = require('./config/database.js');
-//*Setting up config file
+
+//* Setting up config file
 dotenv.config({ path: 'api/config/.env' });
 
-//*Connecting to DB
-connectDatabase();
+async function startServer() {
+  try {
+    //* Connecting to DB
+    await connectDatabase();
 
-app.listen(process.env.PORT, () => {
-  console.log(
-    `Server started on PORT :${process.env.PORT} in ${process.env.NODE_ENV} mode`
-  );
-});
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Server started on PORT :${process.env.PORT} in ${process.env.NODE_ENV} mode`
+      );
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    process.exit(1); // Termina la aplicación con un código de error
+  }
+}
+
+startServer();
