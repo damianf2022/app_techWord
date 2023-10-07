8d4114dc60bb34258ebc740b45285a99566781fd
damianf2022 <damianf2022@gmail.com>
2023-10-07 20:20:50 -0300

    deploy

--- client/src/api_connection/back_call.js
+++ client/src/api_connection/back_call.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const back_call = axios.create({
-	baseURL: 'http://localhost:8080/api/v1'
+	baseURL: 'https://tech-k3o8.onrender.com/api/v1/'
+	// 'http://localhost:8080/api/v1'
 	// 'https://techworld-production.up.railway.app/api/v1/',
 });
 //https://back-deploy-tech-production.up.railway.app/api/v1
